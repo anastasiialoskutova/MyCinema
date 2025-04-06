af462abf865237332b8fe5a73d2f1070a7435a1e
Anastasiia Loskutova <anastasiialoskutova11@gmail.com>
2025-04-06 17:31:27 +0300

    Додано функціональність фільтрації фільмів у компоненті App за допомогою SearchBar та MovieList

--- my-cinema/src/App.jsx
+++ my-cinema/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import MovieList from './components/MovieList';
+import SearchBar from './components/SearchBar';
+import moviesData from './data/movies';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredMovies = moviesData.filter(movie =>
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="app">
+      <h1>Кінотеатр</h1>
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <MovieList movies={filteredMovies} />
+    </div>
+  );
+};
 
-export default App
+export default App;
