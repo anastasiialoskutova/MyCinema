2bcdb7b3782a83d2d72c2c31cb963489746bef05
Anastasiia Loskutova <anastasiialoskutova11@gmail.com>
2025-04-10 14:38:32 +0300

    Додано кнопку бронювання в компонент MovieCard з використанням react-router-dom

--- my-cinema/src/components/MovieCard.jsx
+++ my-cinema/src/components/MovieCard.jsx
@@ -1,14 +1,19 @@
+import { Link } from 'react-router-dom'; // Для роутінгу
+
 const MovieCard = ({ movie }) => {
-    return (
-      <div className="movie-card">
-        <img src={movie.poster} alt={movie.title} />
-        <h3>{movie.title}</h3>
-        <p>{movie.description}</p>
-        <p><strong>Жанр:</strong> {movie.genre}</p>
-        <p><strong>Сеанс:</strong> {movie.time}</p>
-      </div>
-    );
-  };
-  
-  export default MovieCard;
-  
+  return (
+    <div className="movie-card">
+      <img src={movie.poster} alt={movie.title} />
+     <h3>{movie.title}</h3>
+     <p>{movie.description}</p>
+     <p><strong>Жанр:</strong> {movie.genre}</p>
+     <p><strong>Сеанс:</strong> {movie.time}</p>
+      <Link to={`/booking/${movie.id}`}>
+        <button className="book-button">Забронювати</button>
+      </Link>
+    </div>
+  );
+};
+
+export default MovieCard;
+
